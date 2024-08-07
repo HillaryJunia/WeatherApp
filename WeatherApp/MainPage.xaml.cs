70d18d6a70521a8b8df70c6da94cb8ac832ad8b1
danhuy04 <tinonguyen204@gmail.com>
2024-08-07 13:12:52 +0700

    api

---
+++ WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,158 @@
+﻿
+using Microsoft.VisualBasic;
+using WeatherApp.Sevices;
+
+namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public List<Models.List> Weatherlist;
+        private double latitude;
+        private double longitude;
+        public MainPage()
+        {
+            InitializeComponent();
+            Weatherlist = new List<Models.List>();
+
+            InitializeComponent();
+            ForecastDay1.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => ShowForecastDetails(1))
+            });
+            ForecastDay2.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => ShowForecastDetails(2))
+            });
+            ForecastDay3.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => ShowForecastDetails(3))
+            });
+            ForecastDay4.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => ShowForecastDetails(4))
+            });
+            ForecastDay5.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => ShowForecastDetails(5))
+            });
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await GetLocation();
+            await GetWeatherDataByLocation(latitude, longitude);
+
+        }
+
+        public async Task GetLocation()
+        {
+            var location = await Geolocation.GetLocationAsync();
+            latitude = location.Latitude;
+            longitude = location.Longitude;
+        }
+
+        private async void TapLocation_Tapped(object sender, EventArgs e)
+        {
+            await GetLocation();
+            await GetWeatherDataByLocation(latitude, longitude);
+        }
+
+        public async Task GetWeatherDataByLocation(double latitude, double longitude)
+        {
+            var result = await ApiSevice.GetWeather(latitude, longitude);
+            UpdateUI(result);          
+
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            var response = await DisplayPromptAsync(title: "", message: "", placeholder: "Search weather by city", accept: "Search", cancel: "Cancel");
+            if (response != null)
+            {
+                await GetWeatherDataByCity(response);
+            }
+        }
+
+
+        public async Task GetWeatherDataByCity(string city)
+        {
+            var result = await ApiSevice.GetWeatherByCIty(city);
+            UpdateUI(result);
+
+
+        }
+
+        
+
+        
+        public void UpdateUI(dynamic result)
+        {
+            foreach (var item in result.list)
+            {
+                Weatherlist.Add(item);
+            }
+            // CvWeather.ItemsSource = Weatherlist;
+
+            LblCity.Text = result.city.name;
+            LblWeatherDescription.Text = result.list[0].weather[0].description;
+            LblTemperature.Text = result.list[0].main.temperature + "℃";
+            LblHumidity.Text = result.list[0].main.humidity + "%";
+            LblWind.Text = result.list[0].wind.speed + "Km/h";
+            // ImgWeatherIcon.Source = result.list[0].weather[0].customIcon;
+
+            LblCity.Text = result.city.name;
+            for (int i = 1; i <= 5; i++)
+            {
+                var forecast = result.list[i];
+
+                
+                DateTime date = DateTimeOffset.FromUnixTimeSeconds(forecast.dt).DateTime;
+                string dayOfWeek = date.ToString("dddd"); // Lấy thứ trong tuần
+
+                
+                switch (i)
+                {
+                    case 1:
+                        ImgIcon1.Source = $"https://openweathermap.org/img/wn/{forecast.weather[0].icon}.png";
+                        LblDate1.Text = date.ToString("dd/MM/yyyy");
+                        LblDescription1.Text = dayOfWeek;
+                        break;
+                    case 2:
+                        ImgIcon2.Source = $"https://openweathermap.org/img/wn/{forecast.weather[0].icon}.png";
+                        LblDate2.Text = date.ToString("dd/MM/yyyy");
+                        LblDescription2.Text = dayOfWeek;
+                        break;
+                    case 3:
+                        ImgIcon3.Source = $"https://openweathermap.org/img/wn/{forecast.weather[0].icon}.png";
+                        LblDate3.Text = date.ToString("dd/MM/yyyy");
+                        LblDescription3.Text = dayOfWeek;
+                        break;
+                    case 4:
+                        ImgIcon4.Source = $"https://openweathermap.org/img/wn/{forecast.weather[0].icon}.png";
+                        LblDate4.Text = date.ToString("dd/MM/yyyy");
+                        LblDescription4.Text = dayOfWeek;
+                        break;
+                    case 5:
+                        ImgIcon5.Source = $"https://openweathermap.org/img/wn/{forecast.weather[0].icon}.png";
+                        LblDate5.Text = date.ToString("dd/MM/yyyy");
+                        LblDescription5.Text = dayOfWeek;
+                        break;
+                }
+            }
+
+        }  
+
+
+        private void ShowForecastDetails(int dayIndex)
+        {
+            var forecast = Weatherlist[dayIndex];
+            // Cập nhật các Label và các thành phần UI khác với dữ liệu của ngày được chọn
+            // LblCity.Text = forecast.city.name;
+            LblTemperature.Text = forecast.main.temperature + "℃";
+            LblHumidity.Text = forecast.main.humidity + "%";
+            LblWind.Text = forecast.wind.speed + "Km/h";
+            // Cập nhật hình ảnh và mô tả thời tiết
+        }
+    }
+}
---
+++ WeatherApp/Sevice/ApiSevice.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Sevices
+{
+    internal static class ApiSevice
+    {
+        public static async Task<Root> GetWeather(double latitude, double longtime)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(String.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&units=metric&appid=65e5c46f158ea6a4cfc33d8eb8d7a54d", latitude, longtime));
+            return JsonConvert.DeserializeObject<Root>(response)!;
+        }
+
+        public static async Task<Root> GetWeatherByCIty(string city)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(String.Format("https://api.openweathermap.org/data/2.5/forecast?p={0}&units=metric&appid=65e5c46f158ea6a4cfc33d8eb8d7a54d", city));
+            return JsonConvert.DeserializeObject<Root>(response)!;
+        }
+    }
+}
